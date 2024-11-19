2609aa08bce9b7a2c09bbeb25e97bafeff376151
AitorRD <aitroddue@alum.us.es>
2024-11-19 19:30:10 +0100

    fix: resolved conflicts

--- src/routes/doctorRoute.js
+++ src/routes/doctorRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { register, deleteDoctor, getDoctorsBySpeciality} from '../controllers/doctorController.js';
+import { register, deleteDoctor,updateDoctorSpeciality, getDoctorsBySpeciality} from '../controllers/doctorController.js';
 
 const router = express.Router();
 
 router.post('/register', register);
 router.get('/clinic/:clinic/speciality/:speciality?', getDoctorsBySpeciality);
+router.put('/:doctorId/update', updateDoctorSpeciality);
 router.delete('/:id', deleteDoctor);
 
 export default router;
